b4fd45c610391e4c4883c6e8c8eebc1402c1f2b8
deshaeb <butlerdeshae@gmail.com>
2024-10-01 09:17:20 -0400

    Django Lab Part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('dogs/', views.dog_index, name='dog-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Dog:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Lolo', 'tabby', 'Kinda rude.', 3),
+  Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Dog('Bonk', 'selkirk rex', 'Barks loudly.', 6)
+]
+
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Welcome, Doggies!</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the Doggy Collector</h1>')
+  return render(request, 'about.html')
+
+def dog_index(request):
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
